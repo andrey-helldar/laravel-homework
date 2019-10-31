b5891464aeb0c57035edfc10eb6e2975a31073fd
Andrey Helldar <helldar@ai-rus.com>
2019-10-31 19:08:04 +0300

    Added custom translations

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,19 +1,11 @@
 <?php
 
-return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed'   => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
-];
+return array (
+  'checkEmailVerifyLink' => 'Before proceeding, please check your email for a verification link.',
+  'clickHereToRequestAnother' => 'click here to request another',
+  'failed' => 'These credentials do not match our records.',
+  'ifYouNotReceiveEmail' => 'If you did not receive the email',
+  'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+  'verifyEmail' => 'Verify Your Email Address',
+  'verifyFresh' => 'A fresh verification link has been sent to your email address.',
+);
---
+++ resources/lang/en/titles.php
@@ -0,0 +1,11 @@
+<?php
+
+return array (
+  'action' => 'Action',
+  'actions' => 'Actions',
+  'active' => 'Active',
+  'deleted' => 'Deleted',
+  'finished' => 'Finished',
+  'trash' => 'Trash',
+  'trashed' => 'Trashed',
+);
