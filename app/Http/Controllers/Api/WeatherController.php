13f91cba18f779ab1aa6783bcc8a952d591aa35d
Andrey Helldar <helldar@ai-rus.com>
2019-11-11 10:06:42 +0300

    Return of catching errors from Yandex.Weather

--- app/Http/Controllers/Api/WeatherController.php
+++ app/Http/Controllers/Api/WeatherController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Exceptions\Api\ValidationException;
 use App\Http\Controllers\Controller;
 use App\Services\WeatherService;
-
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use function api_response;
@@ -26,16 +26,17 @@ class WeatherController extends Controller
      */
     public function get()
     {
-        //try {
-        $data = $this->service->get();
+        try {
+            $data = $this->service->get();
 
-        return api_response($data);
-        //} catch (ClientException $exception) {
-        //    $content = $exception->getResponse()->getBody()->getContents();
-        //    $decoded = json_decode($content);
-        //    $message = $decoded->message ?? $exception->getMessage();
-        //
-        //    return api_response($message, $exception->getCode());
-        //}
+            return api_response($data);
+        }
+        catch (ClientException $exception) {
+            $content = $exception->getResponse()->getBody()->getContents();
+            $decoded = json_decode($content);
+            $message = $decoded->message ?? $exception->getMessage();
+
+            return api_response($message, $exception->getCode());
+        }
     }
 }
